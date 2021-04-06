e7c571c2356e84996f27055d2b53b57220886840
Сергей Рожко <serg6600@yandex.ru>
2021-04-06 23:04:40 +0300

    first commit

--- script.js
+++ script.js
@@ -17,3 +17,33 @@ console.log(addExpenses.toLowerCase().split(', '));
 let budgetDay = money/30;
 
 console.log(budgetDay);
+
+money = prompt('Ваш месячный доход?');
+
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+
+deposit = confirm('Есть ли у вас депозит в банке?');
+
+let expenses1 = prompt('Введите обязательную статью расходов?');
+let amount1 = prompt('Во сколько это обойдется?');
+let expenses2 = prompt('Введите обязательную статью расходов?');
+let amount2 = prompt('А это во сколько это обойдется?');
+
+let budgetMonth = Number(money) - Number(amount1) - Number(amount2);
+console.log(budgetMonth);
+
+let missionTime = mission/budgetMonth;
+console.log(Math.ceil(missionTime));
+
+budgetDay = budgetMonth/30;
+console.log(Math.ceil(budgetDay));
+
+if (budgetDay > 1200) {
+    console.log('У вас высокий уровень дохода');
+} else if (budgetDay > 600) {
+    console.log('У вас средний уровень дохода');
+} else if (budgetDay >= 0) {
+    console.log('К сожалению у вас уровень дохода ниже среднего');
+} else if (budgetDay < 0) {
+    console.log('Что-то пошло не так');
+}
